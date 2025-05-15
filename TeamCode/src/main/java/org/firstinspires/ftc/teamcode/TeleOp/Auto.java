f855d473b6c1862f42291901c0515f927d6220ea
patrickflynn <mp308mca@gmail.com>
2025-05-15 11:14:19 -0500

    Merge pull request #1416 from FIRST-Tech-Challenge/20250121-174034-release-candidate
    FtcRobotController v10.2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Auto.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class Auto extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //TODO WRITE AN AUTONOMUS PROGRAM
+
+    }
+}
